abaa45c90af45ee25ccbd9c8e1aec6193c881962
Snehamn24 <snehamn216@gmail.com>
2026-05-24 09:01:29 +0530

    Statically typing

--- basicsOf_Dart/basics.dart
+++ basicsOf_Dart/basics.dart
@@ -2,9 +2,36 @@ void main()
 {
   print("Hello World!!!");
 
+  //variables and datatypes
+
+  int age = 30;
+  print(age);
+
+ //String 'S' must be in uppercase
+  String name = "Alex";
+  print(name);
+
+  //name=30;--->error String cannot be assigned with the integer value
+
+
+//changing the string value to other string value it works
+  name = "Ken";
+  print(name);
+
+  //boolean datatye
+  bool isMorning = true;
+  print(isMorning);
+
+  //there is no float datatype in dart instead use dart
+  double a = 3.366;
+  print(a);
+
+
   //loop demonstration using a dart
   for(int i=1;i<=5;i++)
   {
     print('The value is ${i}');
   }
+
+  
 }
